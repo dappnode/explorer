d0bf9a9eb09f2f8f219969a2edc6ad0c16034f54
dapplion <dapplion@users.noreply.github.com>
2020-05-24 18:00:48 +0000

    deploy: 03f05750edcbc18c28fedd81a4eff8d871c758f6

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d83033b7afac51c72ea2c4b102e1e7de.js"
+  "./precache-manifest.dfc5a19799eb90414b7209cccd2a6c9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
